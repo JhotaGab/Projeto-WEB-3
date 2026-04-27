1dfa44a41b8f26f84f4db2035623e25c156e33aa
João <joaomesquita204@gmail.com>
2026-04-27 09:58:32 -0300

    feat:Aprendendo DOM

--- src/js/script.js
+++ src/js/script.js
@@ -37,5 +37,47 @@ const precoProduto = 199.99;
 const desconto = 0.15;
 const precoFinal = precoProduto * (1 - desconto);
 console.log(precoFinal);
-console.log(`R$ ${precoFinal.toFixed(2)}`);
+console.log(`R$ ${precoFinal.toFixed(2)}`); // Crase é utilizada para concatenar seguida do ${}
+
+//DOM (Document Object Model) Modelo de Documento
+//Permite o JavaScript conversar com o navegador para manipular HTML e CSS
+
+//Mudou o texto utilizando o ID
+const titulo = document.getElementById("titulo");
+titulo.innerText = "Dom transformando o texto"
+
+//Mudou cor do texto sem CSS
+titulo.style.color="blue";
+
+//Criação de lista para mostrar no navegador
+const produtos = [
+    {nome:"Teclado Gamer", preco:199,emPromocao:true},
+    {nome:"Mouse", preco:165,emPromocao:false},
+    {nome:"Mouse Pad", preco:49,emPromocao:true},
+    {nome:"Monitor", preco:599,emPromocao:false},
+];
+console.log(produtos)
+
+//vinculou com o ID
+const container=document.getElementById("lista-produtos")
+
+//Criando os Cards e replicando automaticamente
+
+// => arrow phant / uma função executando outra dentro (CALLBACK)
+//MAP verifica os dados / percorre a lista e seleciona o que pode e não 
+// shift + ` + espaço
+
+function produtosCard(lista){
+    const htmlProdutos = lista.map(item => `
+        <div class="card">
+            <h3>${item.nome}</h3>
+            <p>Preço R$ ${item.preco}</p>
+            ${item.emPromocao ? '<span>Promoção</span>': ''}
+        </div>
+        `).join(''); //transforma o ARRAY em uma unica STRING
+
+        container.innerHTML=htmlProdutos;
+
+}
+produtosCard();
 
